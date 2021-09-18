27e6daf10dae5c6b4cb2209314927f68e7f6025e
rasheed1998 <rasheedabdul1998@gmail.com>
2021-09-18 13:58:37 +0530

    2b file

---
+++ 2b.js
@@ -0,0 +1,11 @@
+ //2.b. Write a function which returns array of seven random numbers in a range of 0-9. All the numbers must be unique
+
+ function sevenRandomNumbers() {
+    var arr = [];
+while(arr.length < 8){
+    var r = Math.floor(Math.random() * 10) ;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+return arr;
+}
+ console.log(sevenRandomNumbers())
